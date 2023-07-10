04aecdf67f114e33fced1e94ace74613b8de794d
Varun <varunmukherjee7@gmail.com>
2023-07-10 17:11:21 +0530

    auth

---
+++ src/app/api/redirect/route.ts
@@ -0,0 +1,110 @@
+import { NextRequest } from "next/server";
+import path from "path";
+import { promises as fs } from 'fs';
+
+
+export async function GET(req: NextRequest) {
+    console.log("notion auth api hit");
+
+    try {
+
+        const code = req.nextUrl.searchParams.get("code")
+        const error = req.nextUrl.searchParams.get("error")
+        
+        if(error){
+            return Response.redirect("https://test-project-2-wheat.vercel.app/")
+        }
+        
+        console.log("code:-");
+        console.log(code);
+
+        if(code){
+            const jsonDir = path.join(process.cwd(), 'json');
+            
+            // console.log("code saved");
+
+            const res = await fetch('https://slack.com/api/oauth.v2.access',{
+                method:"POST",
+                body: JSON.stringify({
+                    //@ts-ignore
+                    code: code,
+                    client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+                    client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET                    
+                }),
+            })
+            
+            // console.log(res);
+            const resData = await res.json();
+            console.log("res");
+            console.log(resData);
+            
+            const ops = await fs.writeFile(jsonDir + "/notionToken.json",JSON.stringify({
+                code:code,
+                ...resData
+            }))
+        }
+
+        return Response.redirect("https://test-project-2-wheat.vercel.app/")
+        
+    } catch (error) {
+        console.log("File download error");
+        console.log(error);
+        return new Response(JSON.stringify({
+            status: "Not okey"
+        }))
+    }
+}
+
+export async function POST(req: Request) {
+    console.log("notion auth post api hit");
+
+    try {
+
+        // const code = req.nextUrl.searchParams.get("code")
+        // const error = req.nextUrl.searchParams.get("error")
+        const body = await req.body;
+        
+        console.log("body :-");
+        console.log(body);
+        
+        // if(error){
+        //     return Response.redirect("http://localhost:3000")
+        // }
+        
+        // console.log("code:-");
+        // console.log(code);
+
+        // if(code){
+        //     const jsonDir = path.join(process.cwd(), 'json');
+        //     const ops = await fs.writeFile(jsonDir + "/notionToken.json",JSON.stringify({code: code}))
+
+        //     console.log("code saved");
+
+        //     const res = await fetch("https://api.notion.com/v1/oauth/token",{
+        //         method:"POST",
+        //         body: {
+        //             //@ts-ignore
+        //             code: code,
+        //             grant_type: "authorization_code",
+        //             redirect_uri: "http://localhost:3000/api/notion/redirect"
+    
+        //         }
+        //     })
+            
+        //     console.log(res);
+        //     const resData = await res.json();
+    
+        //     console.log("res");
+        //     console.log(resData);
+        // }
+
+        return Response.redirect("https://test-project-2-wheat.vercel.app/")
+        
+    } catch (error) {
+        console.log("File download error");
+        console.log(error);
+        return new Response(JSON.stringify({
+            status: "Not okey"
+        }))
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      
+      <a href="https://slack.com/oauth/v2/authorize?client_id=5546871361013.5552770712450&scope=app_mentions:read,channels:read,channels:history,chat:write,files:read,im:history,im:write,links:read,mpim:history,users.profile:read&user_scope=admin.conversations:read,channels:history,files:read,identity.basic,groups:history,admin"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
     </main>
   )
 }
